cce6f9d5aea1d9ad9ad336e15a410f4d9144243b
Biriy <biriyasunil25@gmail.com>
2026-01-17 13:07:51 +0530

    chore: add client-provided zoom, contact, and resource links

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,12 +1,15 @@
-export const ZOOM_LINK = "https://zoom.us/your-actual-link";
+export const ZOOM_LINK =
+  "https://us02web.zoom.us/j/5295617868?pwd=MStIWTg1L3MrSnhMd3N5VStxdWZvdz09";
 
-export const WHATSAPP_NUMBER = "919876543210"; // no + sign
-export const CALL_NUMBER = "919876543210";
+export const WHATSAPP_NUMBER = "919712425555";
+export const CALL_NUMBER = "919712425555";
 
 export const RESOURCES = {
-  A1A2: "https://yoursite.com/german-a1-a2",
-  DEGREE: "https://yoursite.com/bachelors-masters",
-  APS: "https://yoursite.com/aps-process",
-  STORIES: "https://yoursite.com/success-stories",
-  LOAN: "https://yoursite.com/education-loan",
+  MASTERS: "https://indogermaneducation.com/masters/",
+  BACHELORS: "https://indogermaneducation.com/bachelors/",
+  STUDIENKOLLEG: "https://indogermaneducation.com/privesh-in-germany/",
+  GERMAN_A1: "https://indogermaneducation.com/a1-german/",
+  WHY_GERMANY: "https://www.youtube.com/watch?v=vI_B_NjWB-c",
+  WHATSAPP_GROUP:
+    "https://chat.whatsapp.com/Iq0jzDZmaT30SmOEin4CPV",
 };
